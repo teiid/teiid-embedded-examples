8b3933fc05fce2835e93191f2dc2402ea6c63b38
kylinsoong <kylinsoong.1214@gmail.com>
2015-09-14 13:25:06 +0800

    add jaxb provider for rest

--- app/customer/README.md
+++ app/customer/README.md
@@ -23,5 +23,14 @@ Deploy `CustomerRESTWebSvc.war` to a running JBoss server(Assume JBoss EAP 6 run
 
 > Note: Customer Service can be accessed via [http://localhost:8080/CustomerRESTWebSvc/MyRESTApplication/customerList](http://localhost:8080/CustomerRESTWebSvc/MyRESTApplication/customerList)
 
+## Available API Test
 
+| **Name** | **API** |
+|:---------|:--------|
+|getCustomers  |http://localhost:8080/customer/customerList  |
+|getCustomerList |http://localhost:8080/customer/getAll |
+|getCustomerByNumber |http://localhost:8080/customer/getByNumber/{customernumber} |
+|getCustomerByName |http://localhost:8080/customer/getByName/{customername} |
+|getCustomerByCity |http://localhost:8080/customer/getByCity/{city} |
+|getCustomerByCountry |http://localhost:8080/customer/getByCountry/{country} |
 
--- app/customer/src/main/java/org/teiid/examples/app/customer/CustomersResource.java
+++ app/customer/src/main/java/org/teiid/examples/app/customer/CustomersResource.java
@@ -23,13 +23,14 @@ package org.teiid.examples.app.customer;
 
 import java.util.List;
 
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/customer")
+@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 public class CustomersResource {
 	
     CustomerData data = new CustomerData();
@@ -43,36 +44,31 @@ public class CustomersResource {
     
     @GET
     @Path("/getAll")
-    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
     public List<Customer>  getCustomerList() {
         return data.getCustomerList();
     }
     
     @GET
-    @Path("/getByNumber")
-    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    public Customer getCustomerByNumber(@QueryParam("customernumber") String customernumber) {
+    @Path("/getByNumber/{customernumber}")
+    public Customer getCustomerByNumber(@PathParam("customernumber") String customernumber) {
         return data.getCustomerByNumber(customernumber);
     }
     
     @GET
-    @Path("/getByName")
-    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    public Customer  getCustomerByName(@QueryParam("customername") String customername) {
+    @Path("/getByName/{customername}")
+    public Customer  getCustomerByName(@PathParam("customername") String customername) {
         return data.getCustomerByName(customername);
     }
     
     @GET
-    @Path("/getByCity")
-    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    public Customer  getCustomerByCity(@QueryParam("city") String city) {
+    @Path("/getByCity/{city}")
+    public Customer  getCustomerByCity(@PathParam("city") String city) {
         return data.getCustomerByCity(city);
     }
     
     @GET
-    @Path("/getByCountry")
-    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    public Customer  getCustomerByCountry(@QueryParam("country") String country) {
+    @Path("/getByCountry/{country}")
+    public Customer  getCustomerByCountry(@PathParam("country") String country) {
         return data.getCustomerByCountry(country);
     }
 }
